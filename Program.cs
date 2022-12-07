b1fc09deb1c62a0557df171dfae77b89c970aba6
Aman-ABJIMA <119652332+Aman-ABJIMA@users.noreply.github.com>
2022-12-07 19:26:35 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,23 @@
-﻿using Microsoft.VisualBasic;
-using System;
-namespace ArrayProgram
-{
-    class A
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Enter Your Name:");
-            string a = Console.ReadLine();
-            
-            string str = null;
-        
-            string vl = "";
-            for (int i = 0; i < a.Length; i++)
-            {
-                for (int j = i + 1; j <= a.Length; j++)
-                {
-                    vl = (vl + a.Substring(i, j - i)) + " ,";
-                }
-            }
-            Console.Write(vl);
+﻿using System;
 
-        }
+public class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("\"Input your date of joining\"\nEnter Year in 4-digits:");
+        var year = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter month in 2-digits:");
+        var month = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter date in 2-digits :");
+        var date = Convert.ToInt32(Console.ReadLine());
+        DateTime dt = new DateTime(year,month,date);
+        Console.Clear();
+        Console.WriteLine("Date of Joining:");
+        Console.WriteLine(dt);
+        Console.WriteLine("Current Date and Time:");
+        DateTime today = DateTime.Now;
+        Console.WriteLine(today);
+        Console.WriteLine("You Spended:"+(today-dt)+"\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b Days                  ");
+  
     }
 }
