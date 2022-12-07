13eb409bf9e7994aa824e7dcfe3f3a3d25926fad
Aman-ABJIMA <119652332+Aman-ABJIMA@users.noreply.github.com>
2022-12-07 19:29:23 +0530

    Add files via upload

---
+++ arraybased.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualBasic;
+using System;
+namespace ArrayProgram
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Name:");
+            string a = Console.ReadLine();
+            //  Console.WriteLine("Enter Length of Array:");
+            //  int size = Convert.ToInt16(Console.ReadLine());
+            //   char[] a = new char[size];
+            string str = null;
+            // Console.WriteLine("Enter {0} elements into an array:",size);
+            //  for(int i=0; i<size; i++)
+            //  {
+            //     a[i]=Convert.ToChar(Console.ReadLine());
+            //  }
+            string vl = "";
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = i + 1; j <= a.Length; j++)
+                {
+                    vl = (vl + a.Substring(i, j - i)) + " ,";
+                }
+            }
+            Console.Write(vl);
+
+        }
+    }
+}
